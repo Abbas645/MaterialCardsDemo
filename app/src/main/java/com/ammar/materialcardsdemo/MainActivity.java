f4419f5f2169a0d8ead56712045d485ed91d9e24
Ammar Githam <ammar_githam@yahoo.com>
2014-07-01 15:58:16 +0530

    Organized imports

--- app/src/main/java/com/ammar/materialcardsdemo/MainActivity.java
+++ app/src/main/java/com/ammar/materialcardsdemo/MainActivity.java
@@ -2,22 +2,21 @@ package com.ammar.materialcardsdemo;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.app.ListFragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v7.widget.RecyclerView;
-
 
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
