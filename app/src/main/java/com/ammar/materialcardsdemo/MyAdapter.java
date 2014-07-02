7435bb630cee7bcfd241efae176d2224b787d022
Ammar Githam <ammar_githam@yahoo.com>
2014-07-02 17:08:03 +0530

    Trying to use Reveal animation.

--- app/src/main/java/com/ammar/materialcardsdemo/MyAdapter.java
+++ app/src/main/java/com/ammar/materialcardsdemo/MyAdapter.java
@@ -1,12 +1,18 @@
 package com.ammar.materialcardsdemo;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.view.ViewAnimationUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +21,7 @@ import java.util.Set;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     private static final float ELEVATION_LOWER = (float) 5;
-    private static final float ELEVATION_HIGHER = (float) 50;
+    private static final float ELEVATION_HIGHER = (float) 100;
     private String[] mDataset;
     private Map<Integer, View> mViewMap;
     private View selected;
@@ -70,6 +76,34 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
                             mViewMap.get(viewPosition).setElevation((float) 5);
                         }
                     }
+
+                    final TextView hiddenTextView = (TextView) view.findViewById(R.id.hidden_text);
+
+                    // get the center for the clipping circle
+                    int cx = (hiddenTextView .getLeft() + hiddenTextView .getRight()) / 2;
+                    int cy = (hiddenTextView .getTop() + hiddenTextView .getBottom()) / 2;
+
+                    Log.d("CardView", cx + " " + cy);
+
+                    // get the final radius for the clipping circle
+                    int finalRadius = hiddenTextView .getWidth();
+
+                    Log.d("CardView", "Radius: " + finalRadius);
+
+                    // create and start the animator for this view
+                    // (the start radius is zero)
+                    ValueAnimator anim =
+                            ViewAnimationUtils.createCircularReveal(hiddenTextView , cx, cy, 0, finalRadius);
+
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                            super.onAnimationStart(animation);
+                            hiddenTextView.setVisibility(View.VISIBLE);
+                        }
+                    });
+
+                    anim.start();
                 }
             }
         });
