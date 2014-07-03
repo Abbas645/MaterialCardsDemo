ad91b3cc3a77c81a0f8ddd97db250b1db8f4f1c2
Ammar Githam <ammar_githam@yahoo.com>
2014-07-03 14:21:30 +0530

    Added FAB to reaveal hidden textview.

--- app/src/main/java/com/ammar/materialcardsdemo/MainActivity.java
+++ app/src/main/java/com/ammar/materialcardsdemo/MainActivity.java
@@ -1,16 +1,24 @@
 package com.ammar.materialcardsdemo;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.graphics.Outline;
+import android.widget.TextView;
+import android.view.ViewAnimationUtils;
 
 public class MainActivity extends Activity {
 
@@ -62,7 +70,75 @@ public class MainActivity extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            RecyclerView mRecyclerView = (RecyclerView) rootView;
+            final FrameLayout frameLayout = (FrameLayout)rootView;
+            RecyclerView mRecyclerView = (RecyclerView) frameLayout.findViewById(R.id.recyclerView);
+            final TextView hiddenTextView = (TextView) frameLayout.findViewById(R.id.hiddenFloatText);
+
+            ImageButton fab = (ImageButton) frameLayout.findViewById(R.id.fab);
+            Outline mOutlineCircle = new Outline();
+            int shapeSize = getResources().getDimensionPixelSize(R.dimen.priamry_fab_height);
+            mOutlineCircle.setRoundRect(0, 0, shapeSize, shapeSize, shapeSize / 2);
+            fab.setOutline(mOutlineCircle);
+            fab.setClipToOutline(true);
+
+            fab.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+
+                    ImageButton view = (ImageButton) v;
+
+                    if (!view.isSelected()) {
+
+                        view.setSelected(true);
+                        view.setImageResource(R.drawable.ic_action_accept);
+
+                        // get the center for the clipping circle
+                        //int cx = (hiddenTextView .getLeft() + hiddenTextView .getRight()) / 2;
+                        //int cy = (hiddenTextView .getTop() + hiddenTextView .getBottom()) / 2;
+
+                        //Log.d("TextView", cx + " " + cy);
+
+                        // get the final radius for the clipping circle
+                        int finalRadius = (int) Math.sqrt((hiddenTextView.getWidth() * hiddenTextView.getWidth()) + (hiddenTextView.getHeight() * hiddenTextView.getHeight()));
+
+                        Log.d("TextView", "Radius: " + finalRadius);
+
+                        // create and start the animator for this view
+                        // (the start radius is zero)
+                        hiddenTextView.setVisibility(View.VISIBLE);
+                        ValueAnimator anim = ViewAnimationUtils.createCircularReveal(hiddenTextView , 0, 0, 0, finalRadius);
+                        anim.start();
+                    }
+                    else {
+                        view.setSelected(false);
+                        view.setImageResource(R.drawable.ic_action_new);
+
+                        // get the center for the clipping circle
+                        int cx = hiddenTextView.getRight();
+                        int cy = hiddenTextView.getBottom();
+
+                        // get the initial radius for the clipping circle
+                        int initialRadius = (int) Math.sqrt((hiddenTextView.getWidth() * hiddenTextView.getWidth()) + (hiddenTextView.getHeight() * hiddenTextView.getHeight()));
+
+                        // create the animation (the final radius is zero)
+                        ValueAnimator anim =
+                                ViewAnimationUtils.createCircularReveal(hiddenTextView, 0, 0, initialRadius, 0);
+
+                        // make the view invisible when the animation is done
+                        anim.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                hiddenTextView.setVisibility(View.INVISIBLE);
+                            }
+                        });
+
+                        // start the animation
+                        anim.start();
+                    }
+                }
+            });
 
             mRecyclerView.setHasFixedSize(true);
 
@@ -75,7 +151,6 @@ public class MainActivity extends Activity {
             // specify an adapter (see also next example)
             mAdapter = new MyAdapter(myDataset);
             mRecyclerView.setAdapter(mAdapter);
-
             return rootView;
         }
     }
